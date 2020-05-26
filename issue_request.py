ca889a11e27b668b0d9a78f78f6b8cad4c91e466
spkuehl <skuehlhorn@gmail.com>
2020-05-26 18:05:08 -0500

    Changed class parameter name

--- issue_request.py
+++ issue_request.py
@@ -1,17 +1,16 @@
 import requests
 
-
 class IssueRequest:
     '''Access the github api to get issues based on label.
     '''
 
-    def __init__(self, label, username, password, filter='all'):
+    def __init__(self, label, email, password, filter='all'):
         self.label = label
         self.filter = filter
-        self.username = username
+        self.email = email
         self.password = password
 
     def get_issues(self):
         r = requests.get('https://api.github.com/issues?filter=%s&labels=%s'%(self.filter, self.label),
-                          auth=(self.username, self.password))
+                          auth=(self.email, self.password))
         return r
