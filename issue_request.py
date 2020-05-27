bdc99c9c1b70cf5b7ff32e478711301f51f9351d
spkuehl <skuehlhorn@gmail.com>
2020-05-26 22:04:09 -0500

    Improved api request

--- issue_request.py
+++ issue_request.py
@@ -4,13 +4,12 @@ class IssueRequest:
     '''Access the github api to get issues based on label.
     '''
 
-    def __init__(self, label, email, password, filter='all'):
+    def __init__(self, label, email, password):
         self.label = label
-        self.filter = filter
         self.email = email
         self.password = password
 
     def get_issues(self):
-        r = requests.get('https://api.github.com/issues?filter=%s&labels=%s'%(self.filter, self.label),
+        r = requests.get('https://api.github.com/search/issues?q=label:"%s"'%(self.label),
                           auth=(self.email, self.password))
         return r
